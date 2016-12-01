5144234d326fd7b7ab97f4b6c8423701d9b79af9
kccsairc <airc@kccs.co.jp>
2016-12-01 16:08:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="multiarow",
+    name="multiarow2",
     version="0.2",
-    url="https://github.com/AlpacaDB/multiarow",
+    url="https://github.com/kccsairc/multiarow",
     description="Multiclass AROW implementation",
     author="AlpacaDB, Inc.",
     license='MIT',
